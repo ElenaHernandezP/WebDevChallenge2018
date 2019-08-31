16cce86b4f20efb727f2203521b1567ac78c38ab
Elena Hernandez <ranaijnebh2o@gmail.com>
2019-08-31 12:44:18 -0500

    register new sidebar

--- Step3/functions.php
+++ Step3/functions.php
@@ -353,7 +353,15 @@ function bones_register_sidebars() {
     'before_title' => '<h2 class="h3 module__title">',
     'after_title' => '</h2>',
   ));
-
+  register_sidebar(array(
+    'id' => 'advertising',
+    'name' => __( $LanguageNameVariable, 'bonestheme' ),
+    'description' => __( $LanguageDescVariable, 'bonestheme' ),
+    'before_widget' => '<div id="%1$s" class="module module--primary cf %2$s">',
+    'after_widget' => '</div>',
+    'before_title' => '<h3 class="h3 module__title">',
+    'after_title' => '</h3>',
+  ))
 } // don't remove this bracket!
 /* DON'T DELETE THIS CLOSING TAG */
 ?>
